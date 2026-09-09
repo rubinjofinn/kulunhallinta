3f39de354c26d28188364c855da21b55659ed9a1
rubinjofinn <152155987+rubinjofinn@users.noreply.github.com>
2026-09-09 14:21:48 +0300

    Add service worker

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE='kulunhallinta-v1';
+const ASSETS=['./','./index.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
